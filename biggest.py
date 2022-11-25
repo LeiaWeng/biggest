a2b381048b7d78a319e8bd6826e85e0581848510
Leia Weng <eeleia@gmail.com>
2022-11-25 12:20:08 +0800

    add new file

---
+++ biggest.py
@@ -0,0 +1,20 @@
+
+
+def biggest(a,b,c):
+    if a>b:
+        最大數=a
+    else:
+        最大數=b
+    if 最大數<c:
+        最大數=c
+    print("最大值是",最大數)
+
+最大數=0
+第一數 = int(input("輸入第一個數字:"))
+第二數 = int(input("輸入第二個數字:"))
+第三數 = int(input("輸入第三個數字:"))
+
+biggest(第一數,第二數,第三數)
+
+
+
